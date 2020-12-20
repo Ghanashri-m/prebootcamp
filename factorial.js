da0113f7c96eaa7f0ebd4c081ea9726a5b040576
Ghanashri M <ghanashri.28@gmail.com>
2020-12-20 10:14:14 +0530

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,28 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic
+  let input = parseInt(userInput[0]);
+  console.log(factorial(input));
+  //end-here
+});
+
+const factorial = (num) => {
+  if (num == 0 || num == 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
